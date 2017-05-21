a9b4a2b033a0925c7c164efbdf09e2647fa4e559
lploc94 <lploc94@gmail.com>
2017-05-21 15:14:24 +0700

    add new service

--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/HocVienService.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/HocVienService.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using DataAccessLayer.Service;
 
 namespace DataAccessLayer
 {
-    class HocVienService
+    class HocVienService : IService
     {
+        public int delete(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insert(string mahv,string hoten,string gioitinh,string diachi,string sdt,)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/IService.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/IService.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace DataAccessLayer.Service
 {
-    public class DAL
+    interface IService
     {
-        
     }
 }
