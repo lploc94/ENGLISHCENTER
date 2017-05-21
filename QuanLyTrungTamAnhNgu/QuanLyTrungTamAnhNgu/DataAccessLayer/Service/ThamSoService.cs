09a12e9e826687551b744f0c2906572c2dea0c6b
lploc94 <lploc94@gmail.com>
2017-05-21 18:14:41 +0700

    Add ThamSoService method

--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/ThamSoService.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/ThamSoService.cs
@@ -9,6 +9,29 @@ namespace DataAccessLayer.Service
 {
     public class ThamsoService
     {
-        
+        public string getLuongCB()
+        {
+            using (QLTTEntities qltt = new QLTTEntities())
+            {
+                var thamso = from r in qltt.THAMSOes select r;
+                return thamso.FirstOrDefault().LUONGCB;
+            }
+        }
+        public int getSiSoToiDa()
+        {
+            using (QLTTEntities qltt = new QLTTEntities())
+            {
+                var thamso = from r in qltt.THAMSOes select r;
+                return (int)thamso.FirstOrDefault().SISOTOIDA;
+            }
+        }
+        public int getSiSoToiThieu()
+        {
+            using (QLTTEntities qltt = new QLTTEntities())
+            {
+                var thamso = from r in qltt.THAMSOes select r;
+                return (int)thamso.FirstOrDefault().SISOTOITHIEU;
+            }
+        }
     }
 }
