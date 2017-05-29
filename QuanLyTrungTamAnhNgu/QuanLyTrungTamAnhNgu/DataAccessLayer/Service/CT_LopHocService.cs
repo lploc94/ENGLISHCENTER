1477236a28e641b9fbae322ac1290496da12bed3
lploc94 <lploc94@gmail.com>
2017-05-29 22:32:00 +0700

    add CTLH and DK in DAL

--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/CT_LopHocService.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/CT_LopHocService.cs
@@ -7,21 +7,122 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Service
 {
-    class CT_LopHocService : IService
+    class CT_LopHocService
     {
-        public int delete(string code)
+        public int delete(string malop,string magv)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    CT_LOPHOC ctlh = qltt.CT_LOPHOC.Where(p => p.MALOP == malop && p.MAGV == magv).FirstOrDefault();
+                    if (ctlh != null)
+                    {
+                        qltt.CT_LOPHOC.Remove(ctlh);
+                        qltt.SaveChanges();
+                        return 1;
+                    }
+                    return 0;
+                }
+            }
+            catch
+            {
+                return 0;
+            } 
         }
 
-        public DataTable get(string code)
+        public DataTable getByMaLop(string malop)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    var ctlhList = from r in qltt.CT_LOPHOC where r.MALOP == malop select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+                    rtnTable.Columns.Add("MAGV", typeof(string));
+                    foreach (CT_LOPHOC ctlh in ctlhList)
+                    {
+                        rtnTable.Rows.Add(ctlh.MALOP, ctlh.MAGV);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public DataTable getByMaGV(string magv)
+        {
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    var ctlhList = from r in qltt.CT_LOPHOC where r.MAGV == magv select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+                    rtnTable.Columns.Add("MAGV", typeof(string));
+                    foreach (CT_LOPHOC ctlh in ctlhList)
+                    {
+                        rtnTable.Rows.Add(ctlh.MALOP, ctlh.MAGV);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public DataTable getAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    var ctlhList = from r in qltt.CT_LOPHOC select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+                    rtnTable.Columns.Add("MAGV", typeof(string));
+                    foreach (CT_LOPHOC ctlh in ctlhList)
+                    {
+                        rtnTable.Rows.Add(ctlh.MALOP, ctlh.MAGV);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public int insert(string malop, string magv)
+        {
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    CT_LOPHOC ctlh = new CT_LOPHOC()
+                    {
+                        STT=qltt.CT_LOPHOC.Count()+1,
+                        MALOP=malop,
+                        MAGV=magv
+                    };
+                    qltt.CT_LOPHOC.Add(ctlh);
+                    return 1;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/DangKyService.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/DataAccessLayer/Service/DangKyService.cs
@@ -7,21 +7,126 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Service
 {
-    class DangKyService : IService
+    class DangKyService 
     {
-        public int delete(string code)
+        public int delete(string mahv,string malop)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    DANGKY dk = qltt.DANGKies.Where(p => p.MALOP == malop && p.MAHV == mahv).FirstOrDefault();
+                    if (dk != null)
+                    {
+                        qltt.DANGKies.Remove(dk);
+                        qltt.SaveChanges();
+                        return 1;
+                    }
+                    return 0;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
         }
 
-        public DataTable get(string code)
+        public DataTable getByMaLop(string malop)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
 
+                    var dkList = from r in qltt.DANGKies where r.MALOP == malop select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MAHV", typeof(string));
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+                    
+                    foreach (DANGKY dk in dkList)
+                    {
+                        rtnTable.Rows.Add(dk.MAHV,dk.MALOP);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public DataTable getByMaHV(string mahv)
+        {
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    var dkList = from r in qltt.DANGKies where r.MAHV == mahv select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MAHV", typeof(string));
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+
+                    foreach (DANGKY dk in dkList)
+                    {
+                        rtnTable.Rows.Add(dk.MAHV, dk.MALOP);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
         public DataTable getAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                    var dkList = from r in qltt.DANGKies select r;
+                    DataTable rtnTable = new DataTable();
+                    rtnTable.Columns.Add("MAHV", typeof(string));
+                    rtnTable.Columns.Add("MALOP", typeof(string));
+
+                    foreach (DANGKY dk in dkList)
+                    {
+                        rtnTable.Rows.Add(dk.MAHV, dk.MALOP);
+                    }
+                    return rtnTable;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public int insert(string mahv, string malop)
+        {
+
+            try
+            {
+                using (QLTTEntities qltt = new QLTTEntities())
+                {
+
+                   DANGKY dk = new DANGKY()
+                    {
+                        STT = qltt.CT_LOPHOC.Count() + 1,
+                        MAHV = mahv,
+                        MALOP = malop,
+                        
+                    };
+                    qltt.DANGKies.Add(dk);
+                    return 1;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
