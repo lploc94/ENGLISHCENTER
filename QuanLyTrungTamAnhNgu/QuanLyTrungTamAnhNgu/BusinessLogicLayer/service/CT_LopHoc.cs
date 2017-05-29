20f50362c46f26e078192e00da96eb5e7527403e
lploc94 <lploc94@gmail.com>
2017-05-29 21:28:23 +0700

    Add DK ang CT_LH

---
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/BusinessLogicLayer/service/CT_LopHoc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.service
+{
+    class CT_LopHoc
+    {
+        public static int delete(string id, string pass, string malop, string magv)
+        {
+            return 1;
+        }
+        public static DataTable getAll(string id, string pass)
+        {
+            DataTable table = new DataTable();
+            return table;
+        }
+        public static DataTable getByMaLop(string id, string pass,string malop)
+        {
+            DataTable table = new DataTable();
+            return table;
+        }
+        public static DataTable getByMaGV(string id, string pass, string magv)
+        {
+            DataTable table = new DataTable();
+            return table;
+        }
+        public static int insert(string id, string pass, string malop, string magv)
+        {
+            return 1;
+        }
+    }
+}
