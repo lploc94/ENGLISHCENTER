90a349d8fa7bd7b9fb949e71b2590e483f42a63e
lploc94 <lploc94@gmail.com>
2017-06-01 01:56:54 +0700

    add findDiemThiByMaLop

--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/BusinessLogicLayer/service/THi.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/BusinessLogicLayer/service/THi.cs
@@ -16,14 +16,25 @@ namespace BusinessLogicLayer.service
         //3 quyền nhân viên kế toán được thêm xóa sửa các bảng GIANGVIEN, HOCPHI, TT_LUONGGV, NHANVIEN, các bảng còn lại chỉ được xem
         public DataTable findDiemThiByMaHV(string id, string pass, string maHv)
         {
-            //if (CheckService.checkID(id, pass) == 1)//kiểm tra id,pass này có đúng không.
+            if (CheckService.checkID(id, pass) == 1)//kiểm tra id,pass này có đúng không.
             {
                 ThiService tsv = new ThiService();
 
                 return tsv.findDiemThiByMaHV(maHv);
 
             }
-            //return null;
+            return null;
+        }
+        public DataTable findDiemThiByMaLop(string id, string pass, string malop)
+        {
+            if (CheckService.checkID(id, pass) == 1)//kiểm tra id,pass này có đúng không.
+            {
+                ThiService tsv = new ThiService();
+
+                return tsv.findDiemThiByMaLop(malop);
+
+            }
+            return null;
         }
 
         public DataTable getAll(string id, string pass)
