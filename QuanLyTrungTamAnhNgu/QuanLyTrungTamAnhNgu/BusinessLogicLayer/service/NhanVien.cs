f8874fa2cfbd7849f0f5cdd06c728679c61b5133
lploc94 <lploc94@gmail.com>
2017-06-01 20:58:22 +0700

    Add NhanVienservice and NhanVien

---
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/BusinessLogicLayer/service/NhanVien.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Service;
+namespace BusinessLogicLayer.service
+{
+    class NhanVien
+    {
+        public int delete(string id, string pass, string manv)
+        {
+            if (CheckService.checkID(id, pass) == 1)
+            {
+                if (CheckService.checkRole(id, 3) == 1 || CheckService.checkRole(id, 0) == 1)
+                {
+                    return new NhanVienService().delete(manv);
+                }
+            }
+            return 0;
+        }
+        public DataTable get(string id, string pass, string manv)
+        {
+            if (CheckService.checkID(id, pass) == 1)
+            {
+               return new NhanVienService().get(manv);
+                
+            }
+            return null;
+            
+        }
+        public DataTable getAll(string id, string pass)
+        {
+            if (CheckService.checkID(id, pass) == 1)
+            {
+                return new NhanVienService().getAll();
+
+            }
+            return null;
+        }
+        public int insert(string id, string password ,string manv, string hoten, int gioitinh, DateTime ngsinh, string diachi, string sdt, string email, double hesoluong, string username, string pass, short phanquyen, bool active)
+        {
+            if (CheckService.checkID(id, password) == 1)
+            {
+                if (CheckService.checkRole(id, 3) == 1 || CheckService.checkRole(id, 0) == 1)
+                {
+                    return new NhanVienService().insert(manv,hoten,gioitinh,ngsinh,diachi,sdt,email,hesoluong,username,pass,phanquyen,active);
+                }
+            }
+            return 0;
+        }
+        public int update(string id, string password, string manv, string hoten, int gioitinh, DateTime ngsinh, string diachi, string sdt, string email, double hesoluong, string username, string pass, short phanquyen, bool active)
+        {
+            if (CheckService.checkID(id, password) == 1)
+            {
+                if (CheckService.checkRole(id, 3) == 1 || CheckService.checkRole(id, 0) == 1)
+                {
+                    return new NhanVienService().update(manv, hoten, gioitinh, ngsinh, diachi, sdt, email, hesoluong, username, pass, phanquyen, active);
+                }
+            }
+            return 0;
+        }
+
+
+
+    }
+}
