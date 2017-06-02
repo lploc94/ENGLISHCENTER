de8ec15a1edb67d3e5e2b685ebe90b648f7ff268
lploc94 <lploc94@gmail.com>
2017-06-02 23:35:43 +0700

    fix bug

--- QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/MainForm.cs
+++ QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/QuanLyTrungTamAnhNgu/MainForm.cs
@@ -27,7 +27,7 @@ namespace QuanLyTrungTamAnhNgu
             Show_Menu(quyen);
             InitCustom();
             HocVien hv = new HocVien();
-            int x = hv.insert(AccountHelper.getAccountId(), AccountHelper.getAccoutPassword(), "HV28", "Nguyen Phuong Nguyen", 1, "Nha cua Nguyen", "0123456789", "pnpuddingkiller@n.com", new DateTime(2017, 6, 10), 1, new DateTime(1995, 6, 10));
+            
            
         }
 
